ce2a89aa8743a9b9a967906fd7748f0b88bc1caa
Lucas G Carvalho <gasparcl.dev@gmail.com>
2021-08-26 16:46:14 -0300

    Comment added.

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from account import Accounts
 
 '''
     Creating an Instance of class:
+    
 '''
 accounts = Accounts()
+
+'''
+Please, select the default chart files to test application.
+
+'''
 
