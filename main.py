c73e0f49b35e8f5e6750d7a1fa860783ee9b5618
Lucas G Carvalho <gasparcl.dev@gmail.com>
2021-08-26 16:51:07 -0300

    Comment added.

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ accounts = Accounts()
 '''
 Please, select the default chart files to test application.
 
+Your new files were saved in root directory as "summarized_df1.xlsx" and "summarized_df2.xlsx"
+
 '''
+
 
